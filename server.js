b2a65b618dc72796dd3c097352de9607f81c5de7
Kusumanjali Koneru and YuvaKishore Chandra <>
2018-01-27 18:41:02 -0600

    Mongodb connection and add application config

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 const express = require('express');
-const app = express();
 const react = require('react');
 const path = require('path');
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/something', (req,res) => {
-     res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
