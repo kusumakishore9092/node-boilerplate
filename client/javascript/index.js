6a2d1c15e3987d850e89836cb1f3a9cbe16655dc
Kusumanjali Koneru and YuvaKishore Chandra <>
2018-02-03 14:05:01 -0600

    Add redux, redux-router and redux dev tools

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
 import ButtonComponent from 'client/javascript/components/ButtonComponent';
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const history = createHistory();
+const middlewares = applyMiddleware(routerMiddleware(history));
+const store = createStore(reducers, composeEnhancers(middlewares));
 
 ReactDOM.render(
-  <ButtonComponent/>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <ButtonComponent/>
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
