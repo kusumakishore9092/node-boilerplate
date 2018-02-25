741f0206723b32c9c88fa295ec03de40dc046fc4
Kusumanjali Koneru and YuvaKishore Chandra <>
2018-02-24 19:15:21 -0600

    Add Utility Nav

--- client/javascript/components/App/index.jsx
+++ client/javascript/components/App/index.jsx
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import Button from 'client/javascript/components/Button';
 import Footer from 'client/javascript/components/Footer';
 import Header from 'client/javascript/components/Header';
+import UtilityNav from 'client/javascript/components/UtilityNav';
 import './index.style';
 
 export default class App extends Component {
   render() {
     return(
         <div>
+          <UtilityNav />
           <Header />
           <Button />
           <Footer />
